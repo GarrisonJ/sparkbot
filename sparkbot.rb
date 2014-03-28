efc2aa5ba2875afe7ae673f7e25c8503c7889255
Garrison Jensen <garrison.jensen@gmail.com>
2014-03-27 23:15:46 -0700

    Fix my bot...

--- sparkbot.rb
+++ sparkbot.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 require "socket"
 require "openssl"
 require "net/http"
 require 'json'
 
 # Variables
-@help_msg     = ""
+@help_msg     = "!spark 1 2 3 4 5 6"
 @server       = "irc.cat.pdx.edu"
 @port         = "6697"
 @nick         = "spark"
-@channel      = "#robots"
+@channel      = "#test-test"
 @channelPass  = "catsonly"
 @graphitePass = "catsonly"
 
@@ -22,32 +23,6 @@ require 'json'
 @irc_server.puts "USER Spark 0 Spark :I iz a bot"
 @irc_server.puts "NICK #{@nick}"
 @irc_server.puts "JOIN #{@channel} #{@channelPass}"
-
-def get_that_graphite_data (whatData)
-  url = "https://arbiter.cat.pdx.edu:8080/render?target=#{whatData}&format=json"
-  uri = URI.parse(url)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request.basic_auth(@graphitePass, @graphitePass)
-  request["Content-Type"] = "application/json"
-  response = http.request(request)
-  response.body
-end
-
-# Output a string of numbers or nill
-def parse_that_graphite_data (whatData)
-  if whatData 
-    (whatData.delete "^1234567890\s").squeeze.lstrip
-  end
-end
-
-def this_is_a_graphite_request? (call)
-  if call
-    call =~ /^[\S]*$/
-  end
-end
 
 def can_I_spark_this? (astring)
     (astring.delete "1234567890\s").chomp.length == 0 
@@ -66,41 +41,24 @@ def spark_it (nums)
   end
 end
 
-def loop_through_reply (reply)
-    if reply
-      howManySplits = 1.0
-      1.upto(10) { |i|
-        if reply.bytesize / i <= 452
-          howManySplits = i
-          break
-        end
-      }
-      p reply.length
-      p howManySplits
-      regex = ".{" + (reply.delete(" ").length/howManySplits.to_f).ceil.to_s + "}"
-      p Regexp.new regex
-      p reply.scan(Regexp.new regex)
-      reply.scan(Regexp.new regex).each { |x|
-        @irc_server.puts "PRIVMSG #{@channel} :" + x
-      }
-    end
-end
-
 until @irc_server.eof? do
   msg = @irc_server.gets
+  if msg =~ /^PING/
+    @irc_server.puts "PONG" 
+  end
   calc = msg.split(":")[2]
   result = nil
-  if calc =~ /^!spark (.+)/  
-    just_the_request = calc.gsub(/^!spark/, '').lstrip.chomp
-    if this_is_a_graphite_request? just_the_request
-      data = get_that_graphite_data just_the_request 
-      just_the_request = parse_that_graphite_data data
-    end
-    if can_I_spark_this? just_the_request
-      result = spark_it just_the_request
-    end
-    if result
-      loop_through_reply result 
-    end
-  end
+	if calc != nil
+		if calc =~ /^!spark (.+)/  
+			nums = calc.gsub(/^!spark/, '').lstrip.chomp
+			if nums == "help"
+				@irc_server.puts "PRIVMSG #{@channel} :" + @help_msg	
+			else can_I_spark_this? nums
+				result = spark_it nums
+			end
+			if result
+				@irc_server.puts "PRIVMSG #{@channel} :" + result
+			end
+		end
+	end
 end
